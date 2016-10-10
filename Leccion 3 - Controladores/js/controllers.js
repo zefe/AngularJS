aa6afffbb81b91e28640fb7f572f9c4daa1998b9
zefetorres <zefetorres@icloud.com>
2016-10-09 20:42:00 -0500

    Leccion 5 - Scope

--- Leccion 3 - Controladores/js/controllers.js
+++ Leccion 3 - Controladores/js/controllers.js
@@ -3,6 +3,17 @@
 var carAppControllers = angular.module('carAppControllers', []);
 
 carAppControllers.controller('carListCtrl', ['$scope',
-	function($scope){
-		alert("Welcome to CarApp");
-	}])
+	function($scope, Car){
+		$scope.cars = [{
+			"name":"New QuashQai",
+			"snippet": "Made With you in mind to give you the highest quality"
+		},
+		{
+			"name":"Veloster",
+			"snippet" : "Sporty like a coupe, Roomy like a sedam."
+		},
+		{
+			"name":"Navara",
+			"snippet":"The Navara is all about freedom including freedom of choise."
+		}];
+	}]);
